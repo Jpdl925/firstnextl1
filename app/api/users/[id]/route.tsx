5871501d3514915f8f5d5ddac0dac8f3fc47f15c
Jpdl925 <jtpg925@gmail.com>
2024-12-12 14:30:53 -0800

    Added Products api

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,7 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+
 
 // interface Props{
 //     params: {id:number};
@@ -12,4 +14,33 @@ export function GET(request: NextRequest,{params}:{params:{id:number}}) {
     if(params.id>10)
         return NextResponse.json({error:"User not found"}, {status:404})
         return NextResponse.json({id:1, name:'jose'})
+}
+
+export async function PUT(request: NextRequest,{params}:{params:{id:number}}){
+    // Validate the request of the body
+    const body = await request.json()
+    // if invalid, return 400
+    // Using new validation variable from schema.ts (layout of user), safeParse determining if it fits the criteria 
+    const validation = schema.safeParse(body);
+    // If validation if not successful (doesn't match), will throw error from schema, Zod
+    if(!validation.success)
+        return NextResponse.json(validation.error.errors,{status:400});
+    // if doesn't exist 404
+    if(params.id > 10)
+        return NextResponse.json({error:"User not found"},{status:404})
+    // Fetch the user with the given id
+    //Update the User if exist
+    return NextResponse.json({id:1, name:body.name})
+    // return the updated user
+
+}
+
+export function DELETE(request: NextRequest,{params}:{params:{id:number}}) {
+    // Fetched user from db
+    // if not found, 404
+    if(params.id > 10)
+        return NextResponse.json({error:"User not found"},{status:404})
+    // Delete the user if found
+    return NextResponse.json({},{status:200});
+    // return 200 response
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -15,3 +15,15 @@ export function GET(request :NextRequest) {
 
     ]);
 }
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    // Validate the data
+    // Check if invalid, return 400
+    // If no name is inserted then returns with 400 error
+    if(!body.name)
+        return NextResponse.json({error:"Name is required"},{status:400})
+    // Else, return the data that was created
+    return NextResponse.json({id:1, name:body.name},{status:201});
+}
+
